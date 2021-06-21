a2128ccb10806b77bc35583979494bee9b487a3f
Tonda Pleskac <pleskac@gmail.com>
2021-06-22 00:59:31 +0200

    Fix redirects

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,133 +12,133 @@ module.exports = {
         typeName: 'NetlifyRedirects',
         redirects: [
           {
-             from: "/16-odklad-posledni-vlny-eet-v-roce-2020",
-             to: "/odklad-posledni-vlny-eet-v-roce-2020/",
+             from: "/articles/16-odklad-posledni-vlny-eet-v-roce-2020",
+             to: "/articles/odklad-posledni-vlny-eet-v-roce-2020/",
              status: 302
           },
           {
-             from: "/17-na-co-se-nas-nejcasteji-ptate",
-             to: "/na-co-se-nas-nejcasteji-ptate/",
+             from: "/articles/17-na-co-se-nas-nejcasteji-ptate",
+             to: "/articles/na-co-se-nas-nejcasteji-ptate/",
              status: 302
           },
           {
-             from: "/19-pravidla-fakturace",
-             to: "/pravidla-fakturace/",
+             from: "/articles/19-pravidla-fakturace",
+             to: "/articles/pravidla-fakturace/",
              status: 302
           },
           {
-             from: "/20-aktualne-bude-dan-z-nabyti-nemovitosti-skutecne-zrusena",
-             to: "/aktualne-bude-dan-z-nabyti-nemovitosti-skutecne-zrusena/",
+             from: "/articles/20-aktualne-bude-dan-z-nabyti-nemovitosti-skutecne-zrusena",
+             to: "/articles/aktualne-bude-dan-z-nabyti-nemovitosti-skutecne-zrusena/",
              status: 302
           },
           {
-             from: "/21-co-musi-obsahovat-faktura",
-             to: "/co-musi-obsahovat-faktura/",
+             from: "/articles/21-co-musi-obsahovat-faktura",
+             to: "/articles/co-musi-obsahovat-faktura/",
              status: 302
           },
           {
-             from: "/22-cislujete-spravne-sve-faktury",
-             to: "/cislujete-spravne-sve-faktury/",
+             from: "/articles/22-cislujete-spravne-sve-faktury",
+             to: "/articles/cislujete-spravne-sve-faktury/",
              status: 302
           },
           {
-             from: "/23-online-fakturace-je-dnes-jiz-standard-jake-jsou-jeji-vyhody",
-             to: "/online-fakturace-je-dnes-jiz-standard-jake-jsou-jeji-vyhody/",
+             from: "/articles/23-online-fakturace-je-dnes-jiz-standard-jake-jsou-jeji-vyhody",
+             to: "/articles/online-fakturace-je-dnes-jiz-standard-jake-jsou-jeji-vyhody/",
              status: 302
           },
           {
-             from: "/24-naklady-ktere-lze-danove-ne-uznat",
-             to: "/naklady-ktere-lze-danove-neuznat/",
+             from: "/articles/24-naklady-ktere-lze-danove-ne-uznat",
+             to: "/articles/naklady-ktere-lze-danove-neuznat/",
              status: 302
           },
           {
-             from: "/25-vyse-zaloh-pro-osvc-v-roce-2020",
-             to: "/vyse-zaloh-pro-osvc-v-roce-2020",
+             from: "/articles/25-vyse-zaloh-pro-osvc-v-roce-2020",
+             to: "/articles/vyse-zaloh-pro-osvc-v-roce-2020",
              status: 302
           },
           {
-             from: "/26-platce-vs-neplatce-dph-kdy-je-vyhodna-dobrovolna-registrace",
-             to: "/platce-vs-neplatce-dph-kdy-je-vyhodna-dobrovolna-registrace/",
+             from: "/articles/26-platce-vs-neplatce-dph-kdy-je-vyhodna-dobrovolna-registrace",
+             to: "/articles/platce-vs-neplatce-dph-kdy-je-vyhodna-dobrovolna-registrace/",
              status: 302
           },
           {
-             from: "/27-lhuty-pro-povinnou-archivaci-faktur-pro-podnikatele-v-cr",
-             to: "/lhuty-pro-povinnou-archivaci-faktur-pro-podnikatele-v-cr/",
+             from: "/articles/27-lhuty-pro-povinnou-archivaci-faktur-pro-podnikatele-v-cr",
+             to: "/articles/lhuty-pro-povinnou-archivaci-faktur-pro-podnikatele-v-cr/",
              status: 302
           },
           {
-             from: "/28-aktualne-dan-z-nabyti-nemovitosti-zrusena",
-             to: "/aktualne-dan-z-nabyti-nemovitosti-zrusena/",
+             from: "/articles/28-aktualne-dan-z-nabyti-nemovitosti-zrusena",
+             to: "/articles/aktualne-dan-z-nabyti-nemovitosti-zrusena/",
              status: 302
           },
           {
-             from: "/29-podpora-fakturaonline-cz",
-             to: "/podpora-fakturaonline-cz/",
+             from: "/articles/29-podpora-fakturaonline-cz",
+             to: "/articles/podpora-fakturaonline-cz/",
              status: 302
           },
           {
-             from: "/30-fakturaonline-spustila-funkci-zobrazeni-stavu-faktur",
-             to: "/fakturaonline-spustila-funkci-zobrazeni-stavu-faktur",
+             from: "/articles/30-fakturaonline-spustila-funkci-zobrazeni-stavu-faktur",
+             to: "/articles/fakturaonline-spustila-funkci-zobrazeni-stavu-faktur",
              status: 302
           },
           {
-             from: "/31-pausalni-dan-pro-osvc-od-roku-2021",
-             to: "/pausalni-dan-pro-osvc-od-roku-2021/",
+             from: "/articles/31-pausalni-dan-pro-osvc-od-roku-2021",
+             to: "/articles/pausalni-dan-pro-osvc-od-roku-2021/",
              status: 302
           },
           {
-             from: "/32-nevite-zda-podnikat-jako-fyzicka-ci-pravnicka-osoba",
-             to: "/nevite-zda-podnikat-jako-fyzicka-ci-pravnicka-osoba/",
+             from: "/articles/32-nevite-zda-podnikat-jako-fyzicka-ci-pravnicka-osoba",
+             to: "/articles/nevite-zda-podnikat-jako-fyzicka-ci-pravnicka-osoba/",
              status: 302
           },
           {
-             from: "/33-nejen-danove-novinky-pro-rok-2021",
-             to: "/nejen-danove-novinky-pro-rok-2021/",
+             from: "/articles/33-nejen-danove-novinky-pro-rok-2021",
+             to: "/articles/nejen-danove-novinky-pro-rok-2021/",
              status: 302
           },
           {
-             from: "/34-vyse-zaloh-pro-osvc-v-roce-2021",
-             to: "/vyse-zaloh-pro-osvc-v-roce-2021",
+             from: "/articles/34-vyse-zaloh-pro-osvc-v-roce-2021",
+             to: "/articles/vyse-zaloh-pro-osvc-v-roce-2021",
              status: 302
           },
           {
-             from: "/35-fakturaonline-pro-nove-uzivatele-jsme-prodlouzili-zkusebni-obdobi",
-             to: "/fakturaonline-pro-nove-uzivatele-jsme-prodlouzili-zkusebni-obdobi/",
+             from: "/articles/35-fakturaonline-pro-nove-uzivatele-jsme-prodlouzili-zkusebni-obdobi",
+             to: "/articles/fakturaonline-pro-nove-uzivatele-jsme-prodlouzili-zkusebni-obdobi/",
              status: 302
           },
           {
-             from: "/36-datova-schranka-jake-jsou-jeji-vyhody-a-nevyhody-jak-si-ji-zaridit",
-             to: "/datova-schranka-jake-jsou-jeji-vyhody-a-nevyhody-jak-si-ji-zaridit/",
+             from: "/articles/36-datova-schranka-jake-jsou-jeji-vyhody-a-nevyhody-jak-si-ji-zaridit",
+             to: "/articles/datova-schranka-jake-jsou-jeji-vyhody-a-nevyhody-jak-si-ji-zaridit/",
              status: 302
           },
           {
-             from: "/37-podpora-v-nezamestanosti-pro-osvc",
-             to: "/podpora-v-nezamestanosti-pro-osvc/",
+             from: "/articles/37-podpora-v-nezamestanosti-pro-osvc",
+             to: "/articles/podpora-v-nezamestanosti-pro-osvc/",
              status: 302
           },
           {
-             from: "/39-je-podnikatelsky-ucet-pro-osvc-nutnosti",
-             to: "/je-podnikatelsky-ucet-pro-osvc-nutnosti/",
+             from: "/articles/39-je-podnikatelsky-ucet-pro-osvc-nutnosti",
+             to: "/articles/je-podnikatelsky-ucet-pro-osvc-nutnosti/",
              status: 302
           },
           {
-             from: "/40-dane-a-danove-priznani-2021-terminy-a-sankce",
-             to: "/dane-a-danove-priznani-2021-terminy-a-sankce/",
+             from: "/articles/40-dane-a-danove-priznani-2021-terminy-a-sankce",
+             to: "/articles/dane-a-danove-priznani-2021-terminy-a-sankce/",
              status: 302
           },
           {
-             from: "/41-prodlouzeni-zkusebni-verze-a-dalsi-novinky-ve-fakturaonline-cz",
-             to: "/prodlouzeni-zkusebni-verze-a-dalsi-novinky-ve-fakturaonline-cz/",
+             from: "/articles/41-prodlouzeni-zkusebni-verze-a-dalsi-novinky-ve-fakturaonline-cz",
+             to: "/articles/prodlouzeni-zkusebni-verze-a-dalsi-novinky-ve-fakturaonline-cz/",
              status: 302
           },
           {
-             from: "/42-zmena-hesla-hromadne-exporty-a-mnoho-dalsiho",
-             to: "/zmena-hesla-hromadne-exporty-a-mnoho-dalsiho/",
+             from: "/articles/42-zmena-hesla-hromadne-exporty-a-mnoho-dalsiho",
+             to: "/articles/zmena-hesla-hromadne-exporty-a-mnoho-dalsiho/",
              status: 302
           },
           {
-             from: "/44-jak-platit-pomoci-qr-kodu-na-fakture",
-             to: "/jak-platit-pomoci-qr-kodu-na-fakture/",
+             from: "/articles/44-jak-platit-pomoci-qr-kodu-na-fakture",
+             to: "/articles/jak-platit-pomoci-qr-kodu-na-fakture/",
              status: 302
           }
         ]
